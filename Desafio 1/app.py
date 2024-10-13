30af170d6e690d9129c2e0b73cec53222bedfcb9
Iuri Souza <iuri.souzasantos@gmail.com>
2024-10-13 10:26:25 -0300

    Desafio 1 curso de automações

---
+++ Desafio 1/app.py
@@ -0,0 +1,7 @@
+import pyautogui
+
+#Mover ate raiz da pasta
+pyautogui.click(5010,678, button='right')
+pyautogui.moveTo(5123,1097, duration=1)
+pyautogui.click(4952,1091, button="left", duration=1)
+
